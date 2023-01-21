519efc53bb393154f29243e21952151a463228d4
Muqaddam <muhammadmustufa555@gmail.com>
2023-01-21 13:21:59 +0500

    changes

--- resources/views/admin/layouts/main.blade.php
+++ resources/views/admin/layouts/main.blade.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
 <html class="loading" lang="{{ app()->getLocale() }}" data-textdirection="ltr">
+
 <head>
-@include('admin.layouts.head')
+    @include('admin.layouts.head')
 
-@yield('styles')
+    @yield('styles')
 </head>
-<body class="vertical-layout vertical-menu-collapsible page-header-dark vertical-modern-menu preload-transitions 2-columns   " data-open="click" data-menu="vertical-modern-menu" data-col="2-columns">
 
-@yield('content')
-@include('admin.layouts.javascript')
+<body
+    class="vertical-layout vertical-menu-collapsible page-header-dark vertical-modern-menu preload-transitions 2-columns   "
+    data-open="click" data-menu="vertical-modern-menu" data-col="2-columns">
 
-@yield('scripts')
+    @yield('content')
+    @include('admin.layouts.javascript')
+
+    @yield('scripts')
 </body>
+
 </html>
