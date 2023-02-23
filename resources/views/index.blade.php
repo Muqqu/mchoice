ee7f8edeaf679d3e5f6d6c7ec8019062c80c0202
Muqaddam <muhammadmustufa555@gmail.com>
2023-02-23 14:59:26 +0500

    changes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,51 +4,48 @@
 @endsection
 
 @section('content')
+    <div class="layout">
 
-        <div class="layout">
+        <!-- Navbar -->
+        @include('partial.navbar')
+        <!-- Navbar -->
 
-            <!-- Navbar -->
-@include('partial.navbar')
-            <!-- Navbar -->
+        <!-- Sidebar -->
+        @include('partial.sidebar')
+        <!-- Sidebar -->
 
-            <!-- Sidebar -->
-@include('partial.sidebar')
-            <!-- Sidebar -->
+        <!-- Main Content -->
+        <div class="main" data-mobile-height="">
 
-            <!-- Main Content -->
-            <div class="main" data-mobile-height="">
+            <!-- Default Page -->
+            <div class="chat flex-column justify-content-center text-center">
+                <div class="container-xxl">
 
-                <!-- Default Page -->
-                <div class="chat flex-column justify-content-center text-center">
-                    <div class="container-xxl">
-
-                        <div class="avatar avatar-lg mb-5">
-                            <img class="avatar-img" src="{{ empty($user->profile) ?
-															asset('storage/index.jpg') :
-															asset(\Storage::url($user->profile))
-														 }}" alt="">
-                        </div>
-
-                        <h6>Hey, {{ explode(' ', $user->name)[0] }}!</h6>
-                        <p>Please select a chat to start messaging.</p>
+                    <div class="avatar avatar-lg mb-5">
+                        <img class="avatar-img"
+                            src="{{ empty($user->profile) ? asset('storage/index.jpg') : asset(\Storage::url($user->profile)) }}"
+                            alt="">
                     </div>
-                </div>
-                <!-- Default Page -->
 
+                    <h6>Hey, {{ explode(' ', $user->name)[0] }}!</h6>
+                    <p>Please select a chat to start messaging.</p>
+                </div>
             </div>
-            <!-- Main Content -->
+            <!-- Default Page -->
 
         </div>
-        <!-- Layout -->
+        <!-- Main Content -->
 
-        <!-- DropzoneJS: Template -->
-@include('partial.dropzone')
-        <!-- DropzoneJS: Template -->
+    </div>
+    <!-- Layout -->
 
-        <!-- Modal: Invite friends -->
-@include('partial.invite')
-        <!-- Modal: Invite friends -->
+    <!-- DropzoneJS: Template -->
+    @include('partial.dropzone')
+    <!-- DropzoneJS: Template -->
 
+    <!-- Modal: Invite friends -->
+    @include('partial.invite')
+    <!-- Modal: Invite friends -->
 @endsection
 
 @section('scripts')
